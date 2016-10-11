e5f4523ba3608da9ddd3525b1005b4e1e59bff67
USER <USER@USER7-PC>
2016-10-11 16:24:05 +0900

    HTML -> JSP

---
+++ bootstrap/WebContent/ch08/member_scheme.sql
@@ -0,0 +1,13 @@
+
+create table member(
+userid 		varchar(16) not null primary key,
+username 	varchar(12) not null,
+password 	varchar(12) not null,
+securitynum 	varchar(14) not null,
+email 		varchar(50),
+zipcode		varchar(7),
+address1	varchar(100),
+address2	varchar(100),
+phone		varchar(13),
+regedit		varchar(50)
+);
